e3384bb95430bc54ce16812bdeb5b018cddc4c5f
hana <hanaabrham89@gmail.com>
2025-04-12 17:47:46 +0200

    feat: implement routing for Home, AboutUs, Recipes, and CookWare components

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,18 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import AboutAs from "../pages/AbouUs";
+import Recipes from "../pages/Recipes";
+import CookWare from "../pages/Cookware";
 
 function AllRouts() {
-    return (
-      <Routes>
-      
-      </Routes>
-    );
-  }
-  
-  export default AllRouts;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about-us" element={<AboutAs />} />
+      <Route path="recipes" element={<Recipes />} />
+      <Route path="/cookware" element={<CookWare />} />
+    </Routes>
+  );
+}
+
+export default AllRouts;
