aedf562b4a8b751a4beed386c6092ed0f56a1627
Ese-Tech <eseosagie22@gmail.com>
2025-04-15 00:19:52 +0200

    Refactor code structure for improved readability and maintainability

--- src/features/recipes/testDummyRecipes.ts
+++ src/features/recipes/testDummyRecipes.ts
@@ -1,9 +1,13 @@
+import image1 from "../../assets/images/homescreenify-sA3wymYqyaI-unsplash.jpg";
+import image2 from "../../assets/images/anna-pelzer-IGfIGP5ONV0-unsplash.jpg";
+import image3 from "../../assets/images/chad-montano-gf9777gaYjs-unsplash.jpg";
+
 export const testDummyRecipes = [
 	{
 		id: "1",
 		title: "Spaghetti Bolognese",
 		description: "A classic Italian pasta dish with rich meat sauce.",
-		image: "https://source.unsplash.com/600x400/?spaghetti",
+		image: image1,
 		category: "Dinner",
 		ingredients: ["spaghetti", "beef", "tomato", "garlic"],
 		instructions: "Boil spaghetti. Cook beef. Mix with tomato and garlic.",
@@ -12,18 +16,19 @@ export const testDummyRecipes = [
 		id: "2",
 		title: "Vegan Salad Bowl",
 		description: "A healthy and refreshing vegan salad bowl.",
-		image: "https://source.unsplash.com/600x400/?salad",
+		image: image2,
 		category: "Vegan",
 		ingredients: ["lettuce", "avocado", "tomato", "chickpeas"],
 		instructions: "Toss all ingredients in a bowl and drizzle with olive oil.",
 	},
 	{
 		id: "3",
-		title: "Banana Pancakes",
-		description: "Fluffy pancakes made with ripe bananas.",
-		image: "https://source.unsplash.com/600x400/?pancakes",
+		title: "Strawberry-Banana Pancakes",
+		description: "Fluffy pancakes made with ripe bananas and strawberries.",
+		image: image3,
 		category: "Breakfast",
-		ingredients: ["banana", "flour", "eggs", "milk"],
-		instructions: "Mash banana. Mix with flour, eggs, milk. Cook on pan.",
+		ingredients: ["banana", "strawberry", "flour", "eggs", "milk"],
+		instructions:
+			"Mash banana. Mix with flour, eggs, milk. Cook on pan and decorate with some strawberries.",
 	},
 ];
