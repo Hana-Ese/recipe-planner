6c7f8cdc07b5023c3debb74615c78a816b32f8a3
Ese-Tech <eseosagie22@gmail.com>
2025-04-14 23:38:45 +0200

    feat: simplify Recipes component by delegating to RecipesPage

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,19 +1,7 @@
-import useRecipes from "../hooks/useRecipes";
-import RecipeCard from "../components/ui/RecipeCard";
+import RecipesPage from "../features/recipes/RecipesPage";
 
 const Recipes = () => {
-  const { recipes, loading, error } = useRecipes();
-
-  if (loading) return <p className="text-center py-6">Loading recipes...</p>;
-  if (error) return <p className="text-center py-6 text-red-500">{error}</p>;
-
-  return (
-    <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} />
-      ))}
-    </div>
-  );
+  return <RecipesPage />;
 };
 
 export default Recipes;
