2443446656e9e07b0b2b9af4c8667c661dd68f2a
hana <hanaabrham89@gmail.com>
2025-04-16 10:14:01 +0200

    feat: implement RecipeApi component to fetch and provide recipe data

---
+++ src/features/recipes/services/RecipeApi.tsx
@@ -0,0 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+import { Recipe, RecipeContextType } from "../../../types/recipe";
+
+const apiRecipe = import.meta.env.VITE_Recipe_api_key;
+console.log("API Key:", apiRecipe);
+const urlBase = `https://api.spoonacular.com/recipes/random?apiKey=${apiRecipe}&number=10`;
+console.log("API Key:", apiRecipe);
+
+export const recipeListProvider = createContext<RecipeContextType>({
+  recipes: [],
+  loading: true,
+  error: null,
+});
+
+function RecipeApi({ children }: { children: React.ReactNode }) {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+
+
+  useEffect(() => {
+    const fetchRecipeList = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(urlBase);
+        const data = await response.json();
+        console.log("API Response:", data);
+        setRecipes(data.recipes || []);
+        setLoading(false);
+      } catch (error) {
+        setError("Failed to fetch recipes");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRecipeList();
+  }, []);
+
+  return (
+    <recipeListProvider.Provider value={{ recipes, loading, error }}>
+      {children}
+    </recipeListProvider.Provider>
+  );
+}
+
+export default RecipeApi;
