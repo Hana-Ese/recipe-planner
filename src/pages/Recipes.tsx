2a33e1e8276bf4c7972aa6275a80cf38127625c9
hana <hanaabrham89@gmail.com>
2025-04-17 11:27:39 +0200

    feat: wrap RecipesPage with RecipeApi and RecipeDietProvider for enhanced context management

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,7 +1,14 @@
 import RecipesPage from "../features/recipes/RecipesPage";
+import RecipeApi from "../features/recipes/services/RecipeApi";
+import RecipeDietProvider from "../hooks/RecipeDietContext";
 
 const Recipes = () => {
-	return <RecipesPage />;
+  return( <RecipeApi>
+	<RecipeDietProvider>
+	  <RecipesPage />
+	</RecipeDietProvider>;
+	</RecipeApi>)
+ 
 };
 
 export default Recipes;
