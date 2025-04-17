a20616166b8b9a03f3ad0169c90c6f588583d89c
hana <hanaabrham89@gmail.com>
2025-04-17 11:33:59 +0200

    feat: add IngredientFilter component for managing ingredient filtering state

---
+++ src/components/ui/IngredientFilter.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function IngredientFilter() {
+      const [ingredientFilter, setIngredientFilter] = useState<string[]>([]);
+      
+    return (  );
+}
+
+export default IngredientFilter;
