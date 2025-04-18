e913ccd3ecb5e81926373c8c16b877fe4579b35f
hana <hanaabrham89@gmail.com>
2025-04-18 12:13:34 +0200

    feat: update RecipeApi to include diet management and fetch recipes based on selected diet

--- src/features/recipes/services/RecipeApi.tsx
+++ src/features/recipes/services/RecipeApi.tsx
@@ -1,33 +1,40 @@
 import { createContext, useEffect, useState } from "react";
 import { Recipe, RecipeContextType } from "../../../types/recipe";
-import { useRecipeDietProvider } from "../../../hooks/RecipeDietContext";
 
-const apiRecipe = import.meta.env.VITE_Recipe_api_key;
+
+const apiRecipeUrl = import.meta.env.VITE_Recipe_api_key2;
 
 export const recipeListProvider = createContext<RecipeContextType>({
   recipes: [],
   loading: true,
   error: null,
+  diet: "",
+  handleDietChange: () => {},
 });
 
 function RecipeApi({ children }: { children: React.ReactNode }) {
+  const [diet, setDiet] = useState("");
+
+  const handleDietChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setDiet(e.target.value);
+  };
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const { diet } = useRecipeDietProvider();
   useEffect(() => {
+    console.log("diet:", diet);
     const fetchRecipeList = async () => {
       setLoading(true);
       try {
-        // const urlBase = `https://api.spoonacular.com/recipes/random?apiKey=${apiRecipe}&number=10&include-tags=${diet}`;
+        const urlBase = `https://api.spoonacular.com/recipes/random?apiKey=${apiRecipeUrl}&number=10&include-tags=${diet}`;
         const response = await fetch(urlBase);
         const data = await response.json();
         console.log("API Response:", data);
         setRecipes(data.recipes || []);
         setLoading(false);
       } catch {
-        setError("Failed to fetch recipes");
+        setError("Failed to get recipes");
       } finally {
         setLoading(false);
       }
@@ -37,7 +44,9 @@ function RecipeApi({ children }: { children: React.ReactNode }) {
   }, [diet]);
 
   return (
-    <recipeListProvider.Provider value={{ recipes, loading, error }}>
+    <recipeListProvider.Provider
+      value={{ recipes, loading, error, handleDietChange, diet }}
+    >
       {children}
     </recipeListProvider.Provider>
   );
