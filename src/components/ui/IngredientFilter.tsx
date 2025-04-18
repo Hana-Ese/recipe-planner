bf02e62dc92bdc64ec309d0f9725582d6123b731
hana <hanaabrham89@gmail.com>
2025-04-18 12:12:36 +0200

    refactor: remove unused IngredientFilter component

--- src/components/ui/IngredientFilter.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function IngredientFilter() {
-      const [ingredientFilter, setIngredientFilter] = useState<string[]>([]);
-      
-    return (  );
-}
-
-export default IngredientFilter;
