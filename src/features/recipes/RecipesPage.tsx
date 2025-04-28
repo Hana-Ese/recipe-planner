22ada0c06426ef31382c484d89203586fc015a0a
hana <hanaabrham89@gmail.com>
2025-04-28 09:23:37 +0200

    feat: display test dummy recipes in RecipesPage component

--- src/features/recipes/RecipesPage.tsx
+++ src/features/recipes/RecipesPage.tsx
@@ -1,19 +1,28 @@
-
 import RecipePlanner from "../../components/ui/RecipePlanner";
 import RecipeDietFilter from "../../components/ui/RecipeDietFilter";
 import { useRecipeListProvider } from "../../hooks/UseRecipeList";
 import RecipesPageContent from "./RecipesPageContent";
 import RecipePlannerApi from "./services/RecipePlannerApi";
+import RecipeCard from "../../components/ui/RecipeCard";
+import { testDummyRecipes } from "./testDummyRecipes";
+console.log(testDummyRecipes);
 
 const RecipesPage = () => {
   const { diet } = useRecipeListProvider();
 
   return (
     <div className="flex flex-row items-center p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {testDummyRecipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
       <RecipeDietFilter />
       {diet && <RecipesPageContent />}
-      <RecipePlannerApi> <RecipePlanner/></RecipePlannerApi >
+      <RecipePlannerApi>
      
+        <RecipePlanner />
+      </RecipePlannerApi>
     </div>
   );
 };
