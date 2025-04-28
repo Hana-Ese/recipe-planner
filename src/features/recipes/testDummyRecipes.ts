d65100b327e29308f842545274c60bbcca54613d
hana <hanaabrham89@gmail.com>
2025-04-28 09:23:49 +0200

    feat: update test dummy recipes with new images and titles

--- src/features/recipes/testDummyRecipes.ts
+++ src/features/recipes/testDummyRecipes.ts
@@ -1,34 +1,34 @@
-import image1 from "../../../assets/images/recipes/spaghetti-bolognese.jpg";
-import image2 from "../../../assets/images/recipes/vegan-salad.jpg";
-import image3 from "../../../assets/images/recipes/strawberry-banana-pancakes.jpg";
+import image1 from "../../assets/images/anna-pelzer-IGfIGP5ONV0-unsplash.jpg"
+import image2 from "../../assets/images/ella-olsson-2x8j0g1a4eE-unsplash.jpg"
+import image3 from "../../assets/images/ella-olsson-2x8j0g1a4eE-unsplash.jpg"
 
 export const testDummyRecipes = [
-	{
-		id: "1",
-		title: "Spaghetti Bolognese",
-		description: "A classic Italian pasta dish with rich meat sauce.",
-		image: image1,
-		category: "Dinner",
-		ingredients: ["spaghetti", "beef", "tomato", "garlic"],
-		instructions: "Boil spaghetti. Cook beef. Mix with tomato and garlic.",
-	},
-	{
-		id: "2",
-		title: "Vegan Salad Bowl",
-		description: "A healthy and refreshing vegan salad bowl.",
-		image: image2,
-		category: "Vegan",
-		ingredients: ["lettuce", "avocado", "tomato", "chickpeas"],
-		instructions: "Toss all ingredients in a bowl and drizzle with olive oil.",
-	},
-	{
-		id: "3",
-		title: "Strawberry-Banana Pancakes",
-		description: "Fluffy pancakes made with ripe bananas and strawberries.",
-		image: image3,
-		category: "Breakfast",
-		ingredients: ["banana", "strawberry", "flour", "eggs", "milk"],
-		instructions:
-			"Mash banana. Mix with flour, eggs, milk. Cook on pan and decorate with some strawberries.",
-	},
+    {
+        id: "1",
+        title: "Classic Italian Pasta",
+        description: "A classic Italian pasta dish with rich meat sauce.",
+        image: image1,
+        category: "Dinner",
+        ingredients: ["spaghetti", "beef", "tomato", "garlic"],
+        instructions: "Boil spaghetti. Cook beef. Mix with tomato and garlic.",
+    },
+    {
+        id: "2",
+        title: "Vegan Salad Bowl",
+        description: "A healthy and refreshing vegan salad bowl.",
+        image: image2,
+        category: "Vegan",
+        ingredients: ["lettuce", "avocado", "tomato", "chickpeas"],
+        instructions: "Toss all ingredients in a bowl and drizzle with olive oil.",
+    },
+    {
+        id: "3",
+        title: "Strawberry-Banana Pancakes",
+        description: "Fluffy pancakes made with ripe bananas and strawberries.",
+        image: image3,
+        category: "Breakfast",
+        ingredients: ["banana", "strawberry", "flour", "eggs", "milk"],
+        instructions:
+            "Mash banana. Mix with flour, eggs, milk. Cook on pan and decorate with some strawberries.",
+    },
 ];
